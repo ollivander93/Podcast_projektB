bc635b4470a418d92ff26b80582b4d7fcba96472
Oliver Sundbäck <oliver.sundback93@gmail.com>
2016-10-11 16:20:11 +0200

    fkjasd
    jdsfad

--- Podcast_projekt/Program.cs
+++ Podcast_projekt/Program.cs
@@ -17,6 +17,7 @@ namespace Podcast_projekt
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //hjeblakdskbflkasdbflhasdbflhasbdlfjhasbldfbasldbhjl
         }
     }
 }
